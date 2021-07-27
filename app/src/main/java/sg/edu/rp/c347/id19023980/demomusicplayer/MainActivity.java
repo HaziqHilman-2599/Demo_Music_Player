36e2e8fa6e9f6d2e32a011053fff541b8411d1e6
19023980 <19023980@myrp.edu.sg>
2021-07-27 14:42:16 +0800

    Initial commit

---
+++ app/src/main/java/sg/edu/rp/c347/id19023980/demomusicplayer/MainActivity.java
@@ -0,0 +1,60 @@
+package sg.edu.rp.c347.id19023980.demomusicplayer;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import androidx.core.content.PermissionChecker;
+
+import android.Manifest;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Environment;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+import java.io.File;
+
+public class MainActivity extends AppCompatActivity {
+    Button btnStart,btnStop;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        int permissionCheck = ContextCompat.checkSelfPermission(MainActivity.this,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE);
+
+        if (permissionCheck != PermissionChecker.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(MainActivity.this,
+                    new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, 0);
+
+        }
+        btnStart = findViewById(R.id.btnStart);
+        btnStop = findViewById(R.id.btnStop);
+
+
+        String folderLocation = Environment.getExternalStorageDirectory().getAbsolutePath() + "/MyFolder";
+        File folder = new File(folderLocation);
+        if (folder.exists() == false){
+            boolean result = folder.mkdir();
+            if (result == true){
+                Log.d("File read/write","folder created");
+            }
+        }
+        btnStart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startService(new Intent(MainActivity.this,MyService.class));
+            }
+        });
+        btnStop.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                stopService(new Intent(MainActivity.this,MyService.class));
+
+            }
+        });
+
+    }
+}
+
